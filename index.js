27868f5dc7fbae1c66e5f33759755695a262a26a
Marius Sprunskas <m.sprunskas@paysera.com>
2023-02-17 15:09:55 +0200

    Fix

--- index.js
+++ index.js
@@ -1,40 +1,23 @@
-// Register service worker to control making site work offline
-
-if ('serviceWorker' in navigator) {
+const registerServiceWorker = async () => {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('/cardsy/sw.js') // TODO: fix prod url
-        .then(() => { console.log('Service Worker Registered'); });
-  }
-}
-
-// Code to handle install prompt on desktop
-
-let deferredPrompt;
-const addBtn = document.querySelector('.add-button');
-addBtn.style.display = 'none';
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI to notify the user they can add to home screen
-  addBtn.style.display = 'block';
-
-  addBtn.addEventListener('click', () => {
-    // hide our user interface that shows our A2HS button
-    addBtn.style.display = 'none';
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
+    try {
+      const registration = await navigator.serviceWorker.register(
+          '/cardsy/sw.js',
+          {
+            scope: './',
+          }
+      );
+      if (registration.installing) {
+        console.log('Service worker installing');
+      } else if (registration.waiting) {
+        console.log('Service worker installed');
+      } else if (registration.active) {
+        console.log('Service worker active');
       }
-      deferredPrompt = null;
-    });
-  });
-});
+    } catch (error) {
+      console.error(`Registration failed with ${error}`);
+    }
+  }
+};
+
+registerServiceWorker();
