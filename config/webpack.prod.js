c614fe34f334be0cf27f00c94832e14a68d108be
Marius <m.sprunskas@gmail.com>
2023-02-19 17:20:06 +0200

    Fix

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -10,7 +10,7 @@ module.exports = merge(common, {
   devtool: false,
   output: {
     path: paths.build,
-    publicPath: '/',
+    publicPath: '/cardsy',
     filename: 'js/[name].[contenthash].bundle.js',
   },
   module: {
